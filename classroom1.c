9593c69e5cb2f30e4dbb6cfcd2bce3b5d46d3b84
napatlnwza <khun.064239@gmail.com>
2025-09-13 21:50:15 +0700

    update

--- classroom1.c
+++ classroom1.c
@@ -1,27 +1,33 @@
 #include <stdio.h>
 // เขียนเอง
-int main() {
-    int date[2];
-    char time[5];
-    printf("Enter date : \n");
-    scanf("%d", &date[0]);
-    printf("Enter time : \n");
+int main()
+{
+    int date;
+    char time[10];
+    printf("Enter date : ");
+    scanf("%d", &date);
+    printf("Enter time : ");
     scanf("%s", time);
-    if (date[0] >= 1 && date[0] <= 31) {
-        if (date[0] == 1 || date[0] == 21 || date[0] == 31) {
-            printf("Today is %d st and  current time is %s", date[0], time);
+    if (date >= 1 && date <= 31)
+    {
+        if (date == 1 || date == 21 || date == 31)
+        {
+            printf("Today is %d st and  current time is %s", date, time);
         }
-        else if (date[0] == 2 || date[0] == 22) {
-            printf("Today is %d nd and  current time is  %s", date[0], time);
+        else if (date == 2 || date == 22)
+        {
+            printf("Today is %d nd and  current time is  %s", date, time);
         }
-        else if (date[0] == 3 || date[0] == 23) {
-            printf("Today is %d rd and  current time is  %s", date[0], time);
+        else if (date == 3 || date == 23)
+        {
+            printf("Today is %d rd and  current time is  %s", date, time);
         }
-        else {
-            printf("Today is %d th and  current time is  %s", date[0], time);
+        else
+        {
+            printf("Today is %d th and  current time is  %s", date, time);
         }
     }
-    if (date[0] < 1 || date[0] >= 32)
+    if (date < 1 || date >= 32)
     {
         printf("Invalid date");
     }
---
+++ classroom3.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main () {
+    cout << "Enter Your name : ";
+    char name[51];
+    cin >> name;
+    cout << "Hello " << name << endl;
+    return 0;
+}
