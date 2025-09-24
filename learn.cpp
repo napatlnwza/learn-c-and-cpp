1cb67be1a52effefea83cf081f5291dfacfa1049
napatlnwza <khun.064239@gmail.com>
2025-09-24 18:24:40 +0700

    test

--- learn.cpp
+++ learn.cpp
@@ -1,22 +1,27 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+// #include <bits/stdc++.h>
 // #include <cmath>
 using namespace std;
 
 int main() {
-    int m[2];
-    cin >> m[0] >> m[1];
-    int matrix[m[0]][m[1]];
-    for (int i=0;i<m[0];i++) {
-        for (int j=0;j<m[1];j++) {
-            cin >> matrix[i][j];
-        }
-    }
-    for (int i=0;i<m[0];i++) {
-        for (int j=0;j<m[1];j++) {
-            cout << matrix[j][i] << " ";
-        }
-        cout << endl;
-    }
+    string x;
+    getline(cin,x);
+    cout << x;
+    // int m[2];
+    // cin >> m[0] >> m[1];
+    // int matrix[m[0]][m[1]];
+    // for (int i=0;i<m[0];i++) {
+    //     for (int j=0;j<m[1];j++) {
+    //         cin >> matrix[i][j];
+    //     }
+    // }
+    // for (int i=0;i<m[0];i++) {
+    //     for (int j=0;j<m[1];j++) {
+    //         cout << matrix[j][i] << " ";
+    //     }
+    //     cout << endl;
+    // }
     // set<string> a;
     // string aa;
     // getline(cin ,aa);
